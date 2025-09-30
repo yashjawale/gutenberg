284eef7f61509182061b0b5c76bdfcde61789006
yashjawale <yash25.j@gmail.com>
2025-09-30 16:20:52 +0530

    refactor: update block comments tests to use data providers

--- phpunit/experimental/block-comments-test.php
+++ phpunit/experimental/block-comments-test.php
@@ -21,11 +21,27 @@ class Gutenberg_Allow_Empty_Block_Comments_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that empty comments are allowed for block_comment_ropen type.
+	 * Data provider for comment types that should allow empty comments.
+	 *
+	 * @return array
 	 */
-	public function test_allows_empty_comment_for_block_comment_ropen() {
+	public function data_provider_resolution_comment_types() {
+		return array(
+			'reopen comment type' => array( 'block_comment_ropen' ),
+			'resolution comment type' => array( 'block_comment_resol' ),
+		);
+	}
+
+	/**
+	 * Tests that empty comments are allowed for resolution comment types.
+	 *
+	 * @dataProvider data_provider_resolution_comment_types
+	 *
+	 * @param string $comment_type The comment type to test.
+	 */
+	public function test_allows_empty_comment_for_resolution_types( $comment_type ) {
 		$prepared_comment = array(
-			'comment_type' => 'block_comment_ropen',
+			'comment_type' => $comment_type,
 		);
 
 		$result = gutenberg_allow_empty_block_comments( false, $prepared_comment );
@@ -34,61 +50,61 @@ class Gutenberg_Allow_Empty_Block_Comments_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that empty comments are allowed for block_comment_resol type.
+	 * Data provider for comment types that should not allow empty comments.
+	 *
+	 * @return array
 	 */
-	public function test_allows_empty_comment_for_block_comment_resol() {
+	public function data_provider_non_resolution_comment_types() {
+		return array(
+			'empty comment type' => array( '' ),
+			'default comment type' => array( 'comment' ),
+			'block comment type' => array( 'block_comment' ),
+			'trackback type' => array( 'trackback' ),
+			'pingback type' => array( 'pingback' ),
+			'custom type' => array( 'some_custom_type' ),
+		);
+	}
+
+	/**
+	 * Tests that empty comments are not allowed for non-resolution comment types.
+	 *
+	 * @dataProvider data_provider_non_resolution_comment_types
+	 *
+	 * @param string $comment_type The comment type to test.
+	 */
+	public function test_does_not_allow_empty_comment_for_non_resolution_types( $comment_type ) {
 		$prepared_comment = array(
-			'comment_type' => 'block_comment_resol',
+			'comment_type' => $comment_type,
 		);
 
 		$result = gutenberg_allow_empty_block_comments( false, $prepared_comment );
 
-		$this->assertTrue( $result );
+		$this->assertFalse( $result, "Empty comments should not be allowed for comment type: {$comment_type}" );
 	}
 
 	/**
-	 * Tests that empty comments are not allowed for other comment types.
+	 * Data provider for testing preservation of original true values.
+	 *
+	 * @return array
 	 */
-	public function test_does_not_allow_empty_comment_for_other_types() {
-		$comment_types = array(
-			'',
-			'comment',
-			'block_comment',
-			'trackback',
-			'pingback',
-			'some_custom_type',
+	public function data_provider_preserve_true_value_scenarios() {
+		return array(
+			'resolution comment type' => array( 'block_comment_ropen' ),
+			'reopen comment type' => array( 'block_comment_resol' ),
+			'regular comment type' => array( 'regular_comment' ),
 		);
-
-		foreach ( $comment_types as $comment_type ) {
-			$prepared_comment = array(
-				'comment_type' => $comment_type,
-			);
-
-			$result = gutenberg_allow_empty_block_comments( false, $prepared_comment );
-
-			$this->assertFalse( $result, "Empty comments should not be allowed for comment type: {$comment_type}" );
-		}
 	}
 
 	/**
 	 * Tests that the function preserves the original allow value when true.
+	 *
+	 * @dataProvider data_provider_preserve_true_value_scenarios
+	 *
+	 * @param string $comment_type The comment type to test.
 	 */
-	public function test_preserves_original_true_value() {
+	public function test_preserves_original_true_value( $comment_type ) {
 		$prepared_comment = array(
-			'comment_type' => 'block_comment_ropen',
-		);
-
-		$result = gutenberg_allow_empty_block_comments( true, $prepared_comment );
-
-		$this->assertTrue( $result );
-	}
-
-	/**
-	 * Tests that the function preserves the original allow value for non-matching types.
-	 */
-	public function test_preserves_original_true_value_for_other_types() {
-		$prepared_comment = array(
-			'comment_type' => 'regular_comment',
+			'comment_type' => $comment_type,
 		);
 
 		$result = gutenberg_allow_empty_block_comments( true, $prepared_comment );
